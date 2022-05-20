0f81d913fbed4915744599f7d81baf52aa06ff79
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-20 13:22:50 +0200

    string manipulation

---
+++ string_manipulation.py
@@ -0,0 +1,8 @@
+# string manipulation in Python
+print("hello, world!\nhello, world")
+# concentination in Python
+print("hello, world" + "jolivet")
+# this what we call indetation error in Python
+#  print()
+# this what we call syntax error in Python
+# print("hello, world)

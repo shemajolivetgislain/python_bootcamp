40b80b28a6a40e51a20a57041d61586fdecd24f6
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-21 15:02:09 +0200

    user input

--- string_manipulation.py
+++ string_manipulation.py
@@ -2,7 +2,12 @@
 print("hello, world!\nhello, world")
 # concentination in Python
 print("hello, world" + "jolivet")
+print('string concentination is done with the "+" sign')
 # this what we call indetation error in Python
 #  print()
 # this what we call syntax error in Python
 # print("hello, world)
+
+# input will user input in console
+# then print will print word "hello" plus user input
+print("hello" + input("enter your name"))

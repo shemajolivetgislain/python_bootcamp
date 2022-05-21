0afa89b8df19019f7994844f5553c3068503b32d
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-21 15:11:37 +0200

    length

--- string_manipulation.py
+++ string_manipulation.py
@@ -1,8 +1,8 @@
 # string manipulation in Python
-print("hello, world!\nhello, world")
-# concentination in Python
-print("hello, world" + "jolivet")
-print('string concentination is done with the "+" sign')
+# print("hello, world!\nhello, world")
+# # concentination in Python
+# print("hello, world" + "jolivet")
+# print('string concentination is done with the "+" sign')
 # this what we call indetation error in Python
 #  print()
 # this what we call syntax error in Python
@@ -10,4 +10,4 @@ print('string concentination is done with the "+" sign')
 
 # input will user input in console
 # then print will print word "hello" plus user input
-print("hello" + input("enter your name"))
+print(  str(len(input("enter your name "))))

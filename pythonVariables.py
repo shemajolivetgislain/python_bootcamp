fca12db0549c127434a2daeccbd49c2193d2efa8
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-23 09:29:40 +0200

    data type function

--- pythonVariables.py
+++ pythonVariables.py
@@ -1,5 +1,8 @@
 name = input("enter your name broo ")
 length = len(name)
 # print(length)
-# printing type of value prompt by a user
-print(type(length))
+# printing type of value  by a user
+# concatinating string with an integer without converting that  integer into an string
+name_length = str(length)
+print("your name length is " + name_length )
+

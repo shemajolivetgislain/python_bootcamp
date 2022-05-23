885f164202e8cea6df90fefbd42f8fc93f24c8c2
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-23 09:31:10 +0200

    adding string with float

--- pythonVariables.py
+++ pythonVariables.py
@@ -5,4 +5,5 @@ length = len(name)
 # concatinating string with an integer without converting that  integer into an string
 name_length = str(length)
 print("your name length is " + name_length )
+print(60 + float(name_length))
 

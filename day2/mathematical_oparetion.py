0ed16fb08cd29738baa8421a4233f3d9406de9a8
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-23 10:17:56 +0200

    mathatical operations

---
+++ day2/mathematical_oparetion.py
@@ -0,0 +1,15 @@
+# PEMDAS
+#   stand for 
+    # paranthesis ()
+    # exponents **
+    # multiplication *
+    # division /
+    # addiction +
+    # substraction -
+    
+# mathematical order in python 
+# 1. ()
+# 2. **
+# 3. * /
+# 4. + -
+print(3 * (3 + 3) / 3 - 3)

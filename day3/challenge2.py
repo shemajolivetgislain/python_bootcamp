e71954892fe04849d6026eb004a4a9648076f0f7
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-24 08:39:22 +0200

    leap year

---
+++ day3/challenge2.py
@@ -0,0 +1,22 @@
+# # days 3 challenge
+# year = int(input('Which year you what TO check?: '))
+# if year % 4 == 0:
+#     print ("It is leap year")
+#     if year % 100 == 0 :
+#         print ("also leap year")
+#     elif year % 400 == 0:
+#         print ("also leap year")
+# else:
+#     print ("it is not leap year")
+    
+# way2 challenge
+year = int(input('Which year you what TO check?: '))
+if year % 4 == 0:
+    if year % 100 == 0 :
+       if year % 400 == 0:
+        print ("leap year")
+       else:
+           print ("Not leap year")
+    else:
+        print ("leap year")
+    print ("Not leap year")  

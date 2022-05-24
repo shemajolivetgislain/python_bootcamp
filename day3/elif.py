669890de3cb384418b0116071467b3f428109a25
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-24 09:22:03 +0200

    multiple if statements

--- day3/elif.py
+++ day3/elif.py
@@ -1,14 +1,23 @@
 print("Welcome to the roalcoast")
 height = int(input("what is your height in cm: "))
+bill= 0
 
 if height > 120:
     print("you can ride the roalcoast")
     age = int(input("what is your age: "))
     if age <= 12:
+        bill = 5
         print("You have to pay 5$")
     elif age<=18:
+        bill = 7
         print("You have to pay 7$")
     else:
+        bill = 12
         print("You have to pay 12$")
+    wants_photo = input("Do you want a photo taken? (y/n): ")
+    if wants_photo == "y":
+        bill += 3
+        print(f"You have to pay {bill}$")
+        
 else:
     print("you can't ride the roalcoast")

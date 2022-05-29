b2da6759372709687d218dd404df32f6d006553e
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-29 13:06:54 +0200

    randimalization

---
+++ day4/randomilization.py
@@ -0,0 +1,11 @@
+import random
+# randomint used to display random numbers from start to the endpoints
+random_integer = random.randint(1, 10)
+print(f"Random integer from 1 to 10  {random_integer}")
+
+
+# used to display random floats numbers btn zero and one
+# btn 0.000000000 and 0.9999999999
+
+random_float = random.random() 
+print(random_float)

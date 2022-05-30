c12a74d054b08a5ec5412a72e3ca467400151293
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-30 13:18:41 +0200

    treasure MAP

--- day4/chellenge4.py
+++ day4/chellenge4.py
@@ -10,3 +10,9 @@ map = [row1, row2, row3]
 
 print(f"{row1} \n {row2} \n {row3}")
 position = input("where do you want to put the treasure? ", )
+horizontal = int(position[0])
+vertical = int(position[1])
+
+map[vertical - 1][horizontal  - 1] = "X"
+
+print(f"{row1} \n {row2} \n {row3}")

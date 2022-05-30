97baea16b68409a0a68f0ccc6445209689636dfa
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-30 12:45:49 +0200

    treasure map

---
+++ day4/chellenge4.py
@@ -0,0 +1,12 @@
+from turtle import position
+from six import print_
+
+
+row1 = ["🥳", "😘", "🥰"]
+row2 = ["🤗", "🤭", "😭"]
+row3 = ["🤑", "🤗", "🥸"]
+
+map = [row1, row2, row3]
+
+print(f"{row1} \n {row2} \n {row3}")
+position = input("where do you want to put the treasure? ", )

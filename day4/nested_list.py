0c2b0767400aed6964512639c110fa86897c425a
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-30 10:11:01 +0200

    combined list

---
+++ day4/nested_list.py
@@ -0,0 +1,6 @@
+vegetables = ["Strawberries", "Spinach", "Kale", "Nectarines"]
+fruits = ["Avocados", "Pineapples", "Cabbages", "Papayas"]
+
+combined_list = [vegetables, fruits]
+
+print(combined_list)

9b634cbc285d932d0f7af306be716d189c952608
shemajolivetgislain <shemajolivet@gmail.com>
2022-05-31 12:40:56 +0200

    day4 final challenge

--- day4/chellenge4.py
+++ day4/chellenge4.py
@@ -13,6 +13,6 @@ position = input("where do you want to put the treasure? ", )
 horizontal = int(position[0])
 vertical = int(position[1])
 
-map[vertical - 1][horizontal  - 1] = "X"
+map[vertical - 1][horizontal - 1] = "X"
 
 print(f"{row1} \n {row2} \n {row3}")

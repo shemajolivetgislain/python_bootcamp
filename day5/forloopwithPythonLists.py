85cc2ecca2c69cfd6eaebb6111fd11575ceb520a
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-01 09:06:00 +0200

    loops with list

---
+++ day5/forloopwithPythonLists.py
@@ -0,0 +1,6 @@
+fruits = ["apples", "orange", "banana"]
+
+for fruit in fruits:
+    print(fruit + " delicious")
+# printing fruits list without indetation will print only list of fruits
+print(fruits)

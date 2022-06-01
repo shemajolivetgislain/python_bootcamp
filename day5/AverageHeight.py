011716c520aaa326501b04c3d5fb3b49391fa4e6
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-01 10:29:03 +0200

    sum and average of number in loops

---
+++ day5/AverageHeight.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+
+total_sum = 0
+average_of_height = 0
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+  total_sum += student_heights[n]
+  average_of_height = total_sum / len(student_heights)
+  print(round(average_of_height))
+ 

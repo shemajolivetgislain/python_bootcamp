5a6c9ac34abb53e773b5e99d0c3ae21b2ad5d27c
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-02 09:42:34 +0200

    summation according to the range of numbers

--- day5/sumofnum.py
+++ day5/sumofnum.py
@@ -1,5 +1,13 @@
-total = 0
+# total = 0
 
-for num in range(1, 100, 2):
-    total += num   
-print(total)
+# for num in range(1, 100, 2):
+#     total += num   
+# print(total)
+
+
+total = 0
+count = int(input("enter your range "))
+numbers = input("enter your numbers ").split()
+for num in range(0, count):
+    total += int(numbers[num])
+print (total)    

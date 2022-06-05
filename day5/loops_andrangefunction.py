8ad5cec4f0d1458f77b18769e3f2903aa171186e
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-05 22:44:12 +0200

    way 2

--- day5/loops_andrangefunction.py
+++ day5/loops_andrangefunction.py
@@ -1,6 +1,13 @@
-sum = 0
+#way1 
+# sum = 0
+# for i in range(1, 101):
+#     if i % 2 == 0:
+#      sum += i
+# print(sum)
 
-for i in range(1, 101):
-    if i % 2 == 0:
-     sum += i
+#way2
+
+sum = 0
+for i in range(1, 101, 2):
+    sum += i
 print(sum)

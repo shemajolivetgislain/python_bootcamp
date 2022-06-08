471355e0ad70b17a3b27d9da11dbefcefea78b8c
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-08 12:20:06 +0200

    list of string with char a

--- day5/name_with_shortname.py
+++ day5/name_with_shortname.py
@@ -1,7 +1,14 @@
-name_list = ["jolivet", "shema", "jiji"]
+# name_list = ["jolivet", "shema", "jiji"]
 
-for i in range(0, len(name_list)):
-    length_name = len(name_list[i])
-    if length_name == 4:
-        print(name_list[i])
-    
+# for i in range(0, len(name_list)):
+#     length_name = len(name_list[i])
+#     if length_name == 4:
+#         print(name_list[i])
+
+
+name_list = ["jolivet", "aa", "jiji"]  
+name_list_a = ""
+for char in name_list:
+    if name_list[char] == "a":
+        name_list_a = name_list[char]
+        print(name_list_a) 

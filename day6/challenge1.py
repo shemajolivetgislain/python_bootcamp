224219a1a146e299209591518028d84a7a1366ef
shemajolivetgislain <shemajolivet@gmail.com>
2022-06-15 23:27:12 +0200

    factorial

--- day6/challenge1.py
+++ day6/challenge1.py
@@ -1,6 +1,13 @@
 # matrix exercises]
 
-for i in range(1, 8 + 1):
-    for j in range(1, i + 1):
-        print(j, end=" ")
-    print("")
+# for i in range(1, 8 + 1):
+#     for j in range(1, i + 1):
+#         print(j, end=" ")
+#     print("")
+
+fact = 1
+number = int(input("enter your number"))
+for i in range(1,number + 1):
+    fact = fact * i
+
+print(fact)
